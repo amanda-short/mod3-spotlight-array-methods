1461ae15a8b2544132e68595d8cbd66bab034766
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-18 12:50:59 -0800

    finished spotlight

--- map.js
+++ map.js
@@ -2,16 +2,29 @@
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+export const getCountryNames = (countries) => countries.map((country) => country.name);
+
+// OR DO THING TO CONSOLE.LOG
+//{const names = countries.map((country) => country.name);
+//   return names;
+// }
+
+/////////////////
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => {
+  return countries.map((list) => [list.name, list.iso2]);
+};
+
+////////////////
 
 // INPUT: the array of shoes from data.js
 // OUTPUT: an array of the brands
 // REQS: use .map
 
-export const getBrands = (brands) => {};
+export const getBrands = (brands) => {
+  return brands.map((list) => list.brand);
+};
